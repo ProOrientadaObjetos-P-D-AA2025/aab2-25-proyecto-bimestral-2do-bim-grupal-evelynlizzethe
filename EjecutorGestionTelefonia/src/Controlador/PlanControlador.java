c26129934ae7edcb652c3280b77f0a2887c54bd9
Evelyn Riofrio <elriofrio1@utpl.edu.ec>
2025-07-24 22:54:29 -0500

    Proyecto

---
+++ EjecutorGestionTelefonia/src/Controlador/PlanControlador.java
@@ -0,0 +1,12 @@
+package Controlador;
+
+import Modelo.PlanMovil;
+import Modelo.PlanPostpago;
+import Modelo.PlanPrepago;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List; 
+
+public class PlanControlador {
+    
+}
